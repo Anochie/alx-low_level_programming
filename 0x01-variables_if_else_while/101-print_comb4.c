fb89f405e8b82356fefd791e11af31248b6b245e
Anochie <25150202+Anochie@users.noreply.github.com>
2022-12-10 15:13:47 -0800

    The success combination in business is: Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,6 @@ int main(void)
 			if (i < j)
 			{
 				putchar(((i / 10) + 48) && ((i % 10)+48));
-				putchar(' ');
 				putchar(((j / 10) + 48) && ((j % 10) + 48));
 				if (i != 98 || j != 99)
 				{
