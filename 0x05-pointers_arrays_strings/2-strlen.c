b55b02b747286acb7cb50049cd982611cc29a725
Anochie <25150202+Anochie@users.noreply.github.com>
2022-12-19 16:02:17 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s[c] != '\0')
+		c++;
+	return c;
+}
